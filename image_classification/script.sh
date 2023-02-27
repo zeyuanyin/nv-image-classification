d15756c17edc6d58b0fba6202beb630294e6750a
zeyuanyin <yinzeyuan1037@gmail.com>
2023-02-27 18:18:24 +0400

    update

---
+++ image_classification/script.sh
@@ -0,0 +1,2 @@
+
+python ./main.py --arch resnet50 --label-smoothing 0.1 --epochs 90 --data-backend pytorch --batch-size 64 --lr 0.64 --lr-schedule cosine --weight-decay 3e-5 --momentum 0.875 --warmup 5 /home/zeyuan.yin/imagenet/
