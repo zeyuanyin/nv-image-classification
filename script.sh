6212af347f4fa6feb4e3dc8b35ae705dd6b7f74f
zeyuanyin <yinzeyuan1037@gmail.com>
2023-02-27 19:03:04 +0400

    ciai

--- script.sh
+++ script.sh
@@ -1,2 +1,1 @@
-
-python ./main.py --arch resnet50 --label-smoothing 0.1 --epochs 90 --data-backend pytorch --batch-size 128 --lr 0.128 --lr-schedule cosine --weight-decay 3e-5 --momentum 0.875 --warmup 5 /home/zeyuan.yin/imagenet/
+python ./main.py --arch resnet50 --label-smoothing 0.1 --epochs 90 --data-backend pytorch --batch-size 256 --lr 0.256 --lr-schedule cosine --weight-decay 3e-5 --momentum 0.875 --warmup 5 /home/zeyuan.yin/imagenet/
