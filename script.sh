2c040bb2e65fc0db88f01b561f4bfaa4eb159f13
zeyuanyin <yinzeyuan1037@gmail.com>
2023-02-28 12:39:37 +0400

    update .sh

--- script.sh
+++ script.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=1GPU                # Job name
-#SBATCH --output=output/myjob-%j.out       # Standard output and error log
-#SBATCH --nodes=1                       # Run all processes on a single node
-#SBATCH --ntasks=1                      # Run on a single CPU
-#SBATCH --mem=40G                       # Total RAM to be used
-#SBATCH --cpus-per-task=64              # Number of CPU cores
-#SBATCH --gres=gpu:1                    # Number of GPUs (per node)
-#SBATCH -p long                         # Use the gpu partition
-#SBATCH --time=10-00:00:00                # Specify the time needed for your experiment
 
 conda activate torch
 nvidia-smi
